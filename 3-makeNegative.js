cd20e80ac3e7a2dca71bd2e714db629b27bc6ffb
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-30 16:25:31 +0500

    makeNegative

---
+++ 3-makeNegative.js
@@ -0,0 +1,10 @@
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+
+function makeNegative(num) {
+    return num>0 ?  num=num*-1 :  num
+}
+// if, else opertorlarni ternada usuli Shart ? to'gri bolsa ishlaydi : Nogri bolsa ishlayd
+// return qaytaradi
