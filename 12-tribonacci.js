18fffd8d2c312b111decec86eb5621e004b59fcf
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-02-01 13:43:44 +0500

    Update 12-tribonacci.js

--- 12-tribonacci.js
+++ 12-tribonacci.js
@@ -12,3 +12,13 @@ function tribonacci(signature, n){
       }
       return arr
 }
+
+function tribonacci2(s,n){
+    var arr = [];
+    for(var i=0; i<n; i++) {
+      arr.push((i<3) ? s[i] : arr[i-1]+arr[i-2]+arr[i-3]);
+    }
+    return arr;
+  }
+
+  // boshqa yo'li'
