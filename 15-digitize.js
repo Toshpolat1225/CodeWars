52c0a4c9ad7582158c5ee9dd74f0d8deab705d8f
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-02-02 12:18:26 +0500

    Update 15-digitize.js

--- 15-digitize.js
+++ 15-digitize.js
@@ -1,3 +1,7 @@
 // 348597 => [7,9,5,8,4,3]
 // 0 => [0]
 const digitize = n => n.toString().split('').map(Number).reverse()
+
+const digitize2 = n => [...String(n)].map(Number).reverse()
+
+// boshqa yo'li
