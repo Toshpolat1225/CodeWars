f23663520220c0594cb5d79280dd8bb3ab48e4f2
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-02-03 13:56:11 +0500

    getSum

---
+++ 20-getSum.js
@@ -0,0 +1,32 @@
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum(a, b) {
+    if(a == b){
+      return a
+    }
+    else{
+        if(b>a){
+            let summ = 0
+            vaqrincha = a
+            for(var i = 0; a<=b; i++) {
+                summ = summ + a
+                a++  
+            }
+            return summ
+        }
+        else{
+            let summ = 0
+            vaqrincha = b
+            for(var i = 0; b<=a; i++) {
+                summ = summ + b
+                b++  
+            }
+            return summ
+        }
+    }
+}
