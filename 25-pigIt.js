40da11a2baa5b2fc28b636825e4e2e642873e205
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-02-05 12:47:55 +0500

    pigIt

---
+++ 25-pigIt.js
@@ -0,0 +1,20 @@
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+function pigIt(str){
+    let array = str.split(" ");
+    let newStr = "";
+  let ay = "ay "
+    for (let i = 0; i < array.length; i++){
+      if(i< array.length-1){
+        ay = ay
+        console.log(array[i].charAt(0)+"ay")
+        if(array[i].charAt(0)=="!"){
+          ay = ""
+        }
+      }else{
+        ay = "ay"
+      }
+    array[i] = array[i].slice(1) + array[i].charAt(0) + ay;
+    newStr += array[i] 
+    } return newStr
+  }
