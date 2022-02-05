46a6515fe21d0ea9fda5e33bede7b682db48c4b9
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-02-05 12:55:12 +0500

    repeatStr

---
+++ 26-repeatStr.js
@@ -0,0 +1,9 @@
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+function repeatStr (n, s) {
+    let soz = s
+    for(var i =0; i<n-1; i++){
+      soz = soz + s.toString()
+    }
+    return soz;
+  }
