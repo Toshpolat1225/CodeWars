b62a4f2cf662abc8f7f36dcea84dd388f900e339
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-02-05 13:09:39 +0500

    alphabetPosition

---
+++ 28-alphabetToNumber.js
@@ -0,0 +1,14 @@
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+    var result = "";
+    for (var i = 0; i < text.length; i++) {
+      var code = text.toUpperCase().charCodeAt(i)
+      if (code > 64 && code < 91) result += (code - 64) + " ";
+    }
+  
+    return result.slice(0, result.length - 1);
+  }
+
+//   charCodeAt() Alifoboni raqamga aylantiradi Alifbo boyicha faqat 64 qoshadi yani A 65 B 66
